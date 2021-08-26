ce59f48973f3ee930d1f42316b79c747db21c65a
Bulik László <laszlo.bulik@ksh.hu>
2021-08-26 16:21:16 +0200

    fixed .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare function interpolate(target: any, context?: { [key: string]: any }, pattern?: RegExp): any;
-declare async function asyncInterpolate(target: any, context?: { [key: string]: any }, pattern?: RegExp): any;
-
-export interpolate;
-export asyncInterpolate;
-
+declare function asyncInterpolate(target: any, context?: { [key: string]: any }, pattern?: RegExp): Promise<any>;
+export {
+    interpolate,
+    asyncInterpolate,
+};
