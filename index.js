7fd3dd63330e1b039d771468d7e1c32ffe69367e
Bulik László <laszlo.bulik@ksh.hu>
2021-08-26 15:29:52 +0200

    added async interpolate

--- index.js
+++ index.js
@@ -35,6 +35,69 @@ const interpolate = (target, context, pattern = /\$\{(.*?[^\\]+?)\}/g) => {
         return o;
     }
     return target;
+};
+
+const AsyncFunction = Object.getPrototypeOf(async function () { }).constructor;
+const asyncEvaluate = (expr, ctx) => AsyncFunction.call(null, "with(this)return (" + expr + ")").call(ctx);
+
+async function asyncReplace(sourceString, regex, asyncReplacer) {
+    regex.lastIndex = 0;
+
+    let match;
+    const matches = [];
+    while ((match = regex.exec(sourceString)) != null) {
+        matches.push(match);
+    }
+
+    const replacements = await Promise.all(matches.map(x => asyncReplacer.apply(null, x)));
+    const sourceParts = sourceString.split(regex).filter((x, i) => i % 2 === 0);
+
+    var result = [];
+    result.push(sourceParts[0]);
+    replacements.forEach((v, i) => {
+        result.push(v);
+        result.push(sourceParts[i + 1]);
+    });
+
+    return result.join('');
 }
 
-module.exports = interpolate;
+const asyncInterpolate = async (target, context, pattern = /\$\{(.*?[^\\]+?)\}/g) => {
+    if (!context) {
+        context = {};
+    }
+    if (typeof target === "string") {
+        try {
+            const match = pattern.exec(target);
+            if (pattern.lastIndex) {
+                pattern.lastIndex = 0; // reset last index if there is a g flag
+            }
+            if (match && match[0].length === target.length) { // eg. "${param}" and not "aaa${param}"
+                return await asyncEvaluate(match[1], context);
+            }
+            return await asyncReplace(target, pattern, async (_, expr) => await asyncEvaluate(expr, context));
+        } catch (ex) {
+            throw `Could not (async) interpolate string "${target}": ${ex}`;
+        }
+    } else if (Array.isArray(target)) {
+        const a = [];
+        for (let value of target) {
+            a.push(await asyncInterpolate(value, context, pattern));
+        }
+        return a;
+    } else if (target && typeof target === "object") {
+        const o = {};
+        for (let p in target) {
+            if (target.hasOwnProperty(p)) {
+                o[p] = await asyncInterpolate(target[p], context, pattern);
+            }
+        }
+        return o;
+    }
+    return target;
+};
+
+module.exports = {
+    interpolate,
+    asyncInterpolate,
+};
